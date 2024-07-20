169c163e4782fdb337a1769166a36fa7a6002988
Sade Nesmith <sadework21@gmail.com>
2024-07-20 12:09:07 -0400

    Initial Commit

---
+++ MVExampleApp/MVExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVExampleAppApp.swift
+//  MVExampleApp
+//
+//  Created by sade on 7/20/24.
+//
+
+import SwiftUI
+
+@main
+struct MVExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
